3e11fd923c63c898a15890419cd9a28346a80c13
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-02-12 18:04:42 +0300

    first commit

---
+++ Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    var name = "charity"
+println(name)
+    var age=23
+    println(age)
+    var phonenumber="o745876412"
+println (phonenumber)
+    var weight=52
+    println(weight)
+    var iskenyan:Boolean=true
+    println(iskenyan)
+    var isnotkenyan:Boolean=false
+    println(isnotkenyan)
+
+}
